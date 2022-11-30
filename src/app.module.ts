ef0ef8d1e26714ea30c3747078b23e4cb6311394
Qeti Khetsuriani <ketideveloper@gmail.com>
2022-11-30 23:08:01 +0400

    auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,12 @@
 import { Module } from '@nestjs/common';
-import { UserModule } from './auth/user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './auth/user/entity/user.entity';
-import { AuthModule } from './auth/auth.module';
-import { ProfileModule } from './profile/profile.module';
 import { TasksModule } from './tasks/tasks.module';
 import { Task } from './tasks/task.entity';
+import { AuthModule } from './auth/auth.module';
+import { User } from './auth/user.entity';
 
 @Module({
   imports: [
-    UserModule,
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: '127.0.0.1',
@@ -17,12 +14,11 @@ import { Task } from './tasks/task.entity';
       username: 'root',
       password: 'password',
       database: 'nestjs',
-      entities: [User, Task],
+      entities: [Task, User],
       synchronize: true,
     }),
-    AuthModule,
-    ProfileModule,
     TasksModule,
+    AuthModule,
   ],
 })
 export class AppModule {}
